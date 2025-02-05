2967f3183f49650cd4ca6192d27eadb47140d05a
Jesus Israel Reynoso Oropeza <muficeti@gmail.com>
2025-02-04 22:41:41 -0600

    Cambios a ser confirmados: 	modificados:     init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ vim.opt.relativenumber=true
 vim.opt.clipboard:append"unnamedplus"
 require("config.lazy")
 dofile('/home/isra/.config/nvim/lua/config/configplugs.lua')
-require("lspconfig").ltex.setup({settings = {
-language = "es"
-}})
+require("lspconfig").ltex.setup({
+	settings = {
+		ltex = {
+			language="es",
+		},
+	},
+})
